b709be2f881409c53ca34b2aa25ff4cbb3888eee
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-09-06 00:01:18 +0200

    Improved interaction with user using commands

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import           TodoList
 
 main :: IO ()
 main = do
-    putStrLn "--- TODO APP v0.1.0 ---"
-    let emptyList = []
-    addItemI emptyList
-    putStrLn "exitting..."
+  putStrLn "--- TODO APP v0.1.0 ---"
+  let emptyList = []
+  interactWithUser emptyList
+
--- src/TodoList.hs
+++ src/TodoList.hs
@@ -1,7 +1,14 @@
 module TodoList where
 
+import           System.IO
+
 type Item = String
 type Items = [Item]
+
+data Command
+  = Quit
+  | DisplayItems
+  | AddItem String
 
 -- Returns a new list of Items with the new item in it
 addItem :: Item -> Items -> Items
@@ -15,13 +22,41 @@ renderItems items =
       renderedItems = zipWith renderItem [1 ..] reversedItems
   in  unlines renderedItems
 
--- Interacts with user, adds new item into given items list
-addItemI :: Items -> IO Items
-addItemI items = do
-  putStrLn "Enter new item to add to your TODO list:"
-  item <- getLine
-  let newItems = addItem item items
-  putStrLn ("Item '" ++ item ++ "' added\n")
-  putStrLn "Your current TODO list is:"
-  putStrLn (renderItems newItems)
-  addItemI newItems
+-- Parses command from user input
+parseCommand :: String -> Either String Command
+parseCommand line = case words line of
+  [         "q"]    -> Right Quit
+  [         "l"]    -> Right DisplayItems
+  "a" : "-" :  item -> Right (AddItem (unwords item))
+  other             -> Left ("unknown command '" ++ unwords other ++ "'")
+
+-- Displays prompt and reads user input
+prompt :: String -> IO String
+prompt text = do
+  putStr text
+  hFlush stdout
+  getLine
+
+-- Handles interaction with user
+interactWithUser :: Items -> IO ()
+interactWithUser items = do
+  putStrLn "\nCommands: q(uit), l(list saved items), a(dd item) - <ITEM NAME>"
+  line <- prompt "> "
+  case parseCommand line of
+    Right Quit -> do
+      putStrLn "Bye!"
+      pure ()
+
+    Right DisplayItems -> do
+      putStrLn "Saved items to do:"
+      putStrLn (renderItems items)
+      interactWithUser items
+
+    Right (AddItem item) -> do
+      let newItems = addItem item items
+      putStrLn ("Item '" ++ item ++ "' added")
+      interactWithUser newItems
+
+    Left error -> do
+      putStrLn ("ERROR: " ++ error)
+      interactWithUser items
